79c8d3a8bdaf0585a5d3f8469771a92ed20007ef
thislyric <ybkim0902@gmail.com>
2018-12-26 16:09:20 -0500

    source codes

---
+++ _site/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><i class="fas fa-cross"></i><span class="highlight"> Blessing</span> Church</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="current" style="margin-top: 10px;"><a href="index.html">Home</a></li>
+            <li  style="margin-top: 10px;"><a href="about.html">About</a></li>
+            <li  style="margin-top: 10px;"><a href="services.html">Services</a></li>
+            <li  style="margin-top: 10px;"><a href="gallery.html">Gallery</a></li>
+            <li>
+              <div class="search-box">
+                <input class="search-txt" type="text" name="" placeholder="Search">
+                <a class="search-btn" href="index.html">
+                <i class="fas fa-search"></i>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </body>
+</html>
